4d9a547b02afef18f3285dd88e6430fde8626619
Max <pixelcat2211@gmail.com>
2023-05-10 22:47:45 +0500

    pagination solution

--- collectors/base_scraper.py
+++ collectors/base_scraper.py
@@ -1,6 +1,9 @@
 from selenium.webdriver.firefox.service import Service as FirefoxService
 from selenium.webdriver import Firefox
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+import time
 
 
 class BaseScraper:
@@ -28,12 +31,35 @@ class BaseScraper:
         city_li = self.driver.find_element(By.XPATH, '/html/body/div[3]/div[1]/div/div[1]/div[1]/div/ul[1]/li[9]/a')
         city_li.click()
 
+        # /html/body/div[1]/div[5]/div/div[2]/div/ul/li[2]
+
+    def __load_all_reviews(self):
+        '''
+        После загрузки страницы товара переходит на вкладку отзывов и загружает все отзывы
+        '''
+        reviews_button = self.driver.find_element(By.XPATH, '/html/body/div[1]/div[5]/div/div[2]/div/ul/li[2]')
+        reviews_button.click()
+        while True:
+            try:
+                button = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'a.reviews__view-change:nth-child(1)')))
+
+                # more_button = self.driver.find_element(By.CSS_SELECTOR, 'a.reviews__view-change:nth-child(1)')
+
+                # more_button.click()
+                # time.sleep(1)
+
+                button.click()
+            except:
+                # если кнопка не найдена, то выходим из цикла
+                break
+
     def collect_data(self, url_source: str):
         if not self.driver:
             self.__start_browser()
         self.__navigate_to_website(url=url_source)
         
         self.__solve_location_dialog()
+        self.__load_all_reviews()
 
 
     def close_browser(self):
--- manage.py
+++ manage.py
@@ -3,4 +3,4 @@ from collectors.base_scraper import BaseScraper
 
 if __name__ == '__main__':
     scraper = BaseScraper()
-    scraper.collect_data('https://kaspi.kz/shop/c/smartphones/?sort=rating')
+    scraper.collect_data('https://kaspi.kz/shop/p/xiaomi-redmi-10c-4-gb-128-gb-goluboi-104417308/?c=750000000#!/item')
