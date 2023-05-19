ae6d6b5326aa46e51b425edd86323c115beb1135
Max <pixelcat2211@gmail.com>
2023-05-19 16:31:58 +0500

    add category parse

--- collectors/base_scraper.py
+++ collectors/base_scraper.py
@@ -3,6 +3,7 @@ from selenium.webdriver import Firefox
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from collections import Counter
 import time
 
 
@@ -12,11 +13,13 @@ class BaseScraper:
             executable_path='Z:\alryum\python_projects\kaspi_bot\drivers\geckodriver.exe')
         self.driver = None
 
+
     def __start_browser(self):
         '''
         Инициализирует драйвер
         '''
         self.driver = Firefox(service=self.service)
+
 
     def __navigate_to_website(self, url):
         '''
@@ -24,14 +27,17 @@ class BaseScraper:
         '''
         self.driver.get(url=url)
 
+
     def __solve_location_dialog(self):
         '''
         Во всплывающем окне выбирает город Алматы
         '''
-        city_li = self.driver.find_element(By.XPATH, '/html/body/div[3]/div[1]/div/div[1]/div[1]/div/ul[1]/li[9]/a')
-        city_li.click()
+        try:
+            city_li = self.driver.find_element(By.XPATH, '/html/body/div[3]/div[1]/div/div[1]/div[1]/div/ul[1]/li[9]/a')
+            city_li.click()
+        except:
+            pass
 
-        # /html/body/div[1]/div[5]/div/div[2]/div/ul/li[2]
 
     def __load_all_reviews(self):
         '''
@@ -42,24 +48,58 @@ class BaseScraper:
         while True:
             try:
                 button = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'a.reviews__view-change:nth-child(1)')))
-
-                # more_button = self.driver.find_element(By.CSS_SELECTOR, 'a.reviews__view-change:nth-child(1)')
-
-                # more_button.click()
-                # time.sleep(1)
-
                 button.click()
             except:
-                # если кнопка не найдена, то выходим из цикла
                 break
 
-    def collect_data(self, url_source: str):
+    
+    def __get_amount_of_new_reviews(self):
+        reviews = self.driver.find_elements(By.CLASS_NAME, 'reviews__date')
+        reviews_date_list = []
+        for i in reviews:
+            reviews_date_list.append(i.text[3:])
+        date_counts = Counter(reviews_date_list)
+        reviews_counter = 0
+        for date, count in date_counts.items():
+            if '2023' in str(date):
+                reviews_counter += int(count)
+        
+        return reviews_counter
+
+
+
+    def get_product_links_list(self, url, amount_of_pages):
+        '''
+        собирает ссылки на все товары в категории, до необходимой страницы
+        '''
+        if not self.driver:
+            self.__start_browser()
+        self.__navigate_to_website(url)
+        self.__solve_location_dialog()
+        links_list = []
+
+        for i in range(amount_of_pages):
+            product_list = self.driver.find_elements(By.CLASS_NAME, 'item-card__image-wrapper')
+            links_list.extend([i.get_attribute('href') for i in product_list])
+            try:
+                next_btn = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, '#scroll-to > div.item-cards-grid > div.pagination > li:nth-child(7)')))
+                next_btn.click()
+            except:
+                break  
+        
+        return links_list
+
+
+    def collect_data(self, url_source):
         if not self.driver:
             self.__start_browser()
         self.__navigate_to_website(url=url_source)
         
         self.__solve_location_dialog()
         self.__load_all_reviews()
+        if self.__get_amount_of_new_reviews() >= 10:
+            return True
+        return False
 
 
     def close_browser(self):
--- manage.py
+++ manage.py
@@ -3,4 +3,12 @@ from collectors.base_scraper import BaseScraper
 
 if __name__ == '__main__':
     scraper = BaseScraper()
-    scraper.collect_data('https://kaspi.kz/shop/p/xiaomi-redmi-10c-4-gb-128-gb-goluboi-104417308/?c=750000000#!/item')
+    # scraper.collect_data('https://kaspi.kz/shop/p/xiaomi-redmi-10c-4-gb-128-gb-goluboi-104417308/?c=750000000#!/item')
+    p_links = scraper.get_product_links_list(
+        'https://kaspi.kz/shop/c/home%20equipment/', 1)
+    result_list = []
+    for i in p_links:
+        if scraper.collect_data(i):
+            result_list.append(i)
+
+    print(result_list)
