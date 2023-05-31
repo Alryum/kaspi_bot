e356aa2e6c7fb9f7b2d2cf35aa6c60d14cdc6269
Max <pixelcat2211@gmail.com>
2023-05-31 23:26:47 +0500

    output.txt

--- manage.py
+++ manage.py
@@ -2,13 +2,18 @@ from collectors.base_scraper import BaseScraper
 
 
 if __name__ == '__main__':
+    print('Starting...')
     scraper = BaseScraper()
-    # scraper.collect_data('https://kaspi.kz/shop/p/xiaomi-redmi-10c-4-gb-128-gb-goluboi-104417308/?c=750000000#!/item')
+    print('Init complete')
     p_links = scraper.get_product_links_list(
-        'https://kaspi.kz/shop/c/home%20equipment/', 1)
+        'https://kaspi.kz/shop/c/wireless%20chargers/?q=%3AavailableInZones%3AMagnum_ZONE1%3Acategory%3AWireless%20chargers&sort=rating&sc&page=1', 4)
     result_list = []
     for i in p_links:
+        print(f'working with {i}')
         if scraper.collect_data(i):
             result_list.append(i)
 
-    print(result_list)
+    scraper.close_browser()
+
+    with open('output.txt', 'w') as file:
+        file.writelines('\n'.join(result_list))
